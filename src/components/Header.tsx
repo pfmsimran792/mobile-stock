9cf7fb5235708f4a347a1361bd7a7729aa9ee080
pfmsimran792 <pfmsimran792@gmail.com>
2026-07-06 15:51:12 +0530

    Initial React project setup

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <header className="bg-blue-600 text-white shadow-md">
+            <div className="max-w-5xl mx-auto p-4">
+                <h1 className="text-2xl font-bold">
+                    📱 Mobile Shop Stock Manager
+                </h1>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div className="max-w-5xl mx-auto p-6">
+            Home Page
+        </div>
+    );
+}
+
+export default Home;
